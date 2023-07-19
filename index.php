19fb99aa677e755121469ae6e954abaea967dca4
DouglasOliveira <douglasoliveiradaluz10@gmail.com>
2023-07-19 11:25:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="teste.css">
+    <title>Mapa mental</title>
+</head>
+
+<header>
+    <h1>Mapa Mental</h1>
+</header>
+
+<body>
+    <div id="pp">
+    
+</div>
+    <div id="tela">
+  <p>Projeto profissional</p>
+    
+</div>
+        </div>
+        <div id="conteudo">
+<div class="ano">    
+    <h1>Hard Skill e Soft skills</h1>
+  
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas vitae scel<span id="dots">...</span><span id="more">erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.</span></p>
+
+<button onclick="myFunction()" id="myBtn">Leia Mais</button>
+</div>        
+
+<div class="preto2"> 
+
+   
+</div>
+<div class="preto3">
+    
+</div>    
+<div class="preto4">
+    
+</div>
+<div>
+
+
+
+</div>
+</div>
+<script src="script.js"></script>
+</body>
+</html>
