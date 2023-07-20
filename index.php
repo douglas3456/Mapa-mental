2df4b8f058369bb640289bfde612859be8d3ce5f
DouglasOliveira <douglasoliveiradaluz10@gmail.com>
2023-07-20 13:53:26 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -13,6 +13,17 @@
 </header>
 
 <body>
+
+<div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>
     <div id="pp">
     
 </div>
@@ -125,9 +136,17 @@
     <p>Jogo de tabuleiro sobre conteudos aprendidos nos ultimos anos </p>
 
 </div>
+<div vw class="enabled">
+            <div vw-access-button class="active"></div>
+            <div vw-plugin-wrapper>
+              <div class="vw-plugin-top-wrapper"></div>
+            </div>
+          
 
 </div>
 </div>
+
+
 <script src="script.js"></script>
 </body>
 </html>
